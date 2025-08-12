5720481d6cbfcd6ac3b4ddb50345d9bfb617040c
bluebirz <9207891+bluebirz@users.noreply.github.com>
2025-08-12 20:17:52 +0200

    revert part 6 + add part 7

--- part-6-cicd/src/storage.tf
+++
@@ -1,5 +0,0 @@
-resource "google_storage_bucket" "bucket_by_maps" {
-  for_each = toset(["bluebirz-bucket1", "bluebirz-bucket2", "bluebirz-bucket3"])
-  name     = each.value
-  location = "US"
-}
--- part-6-cicd/src/variables.tf
+++ part-6-cicd/src/variables.tf
@@ -2,6 +2,6 @@ variable "my_project" {
   type    = string
   default = "bluebirz-playground"
 }
-# variable "my_bucket" { type = string }
-# variable "my_bucket_location" { type = string }
-# variable "my_bucket_class" { type = string }
+variable "my_bucket" { type = string }
+variable "my_bucket_location" { type = string }
+variable "my_bucket_class" { type = string }
